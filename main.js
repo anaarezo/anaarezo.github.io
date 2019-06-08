dd0873b836c451edb8b6c844da74339392ad1fed
Ana Laura Arezo E Silva Rodrigues <ana.rodrigues@grupoboticario.com.br>
2019-06-08 13:07:49 -0300

    Cam

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ function yourselfie(){
             video: true,
             audio: false
         }, function (stream){
+            video.srcObject = stream;
+            video.play();
             //console.log(stream);
-            window.URL = window.URL || window.webkitURL;
+            /**window.URL = window.URL || window.webkitURL;
             var streamURL = window.srcObject(stream);
             yourselfieCam.src = streamURL;
-            yourselfieCam.play();
+            yourselfieCam.play();*/
         }, function (error){
             console.warn(error);
         });
