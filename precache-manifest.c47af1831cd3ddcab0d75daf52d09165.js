6778d9197e6d9d4e12ede182753402bba0583eef
Ana Laura Arezo E Silva Rodrigues <ana.rodrigues@grupoboticario.com.br>
2019-09-13 13:41:16 -0300

    Create: Porftolio

---
+++ precache-manifest.c47af1831cd3ddcab0d75daf52d09165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8aff31fecf3eb9e354d55e65c6653bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "247f2a50408ed7b821ea",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dfb86b5536a935ceeaab",
+    "url": "/static/js/2.7bac774b.chunk.js"
+  },
+  {
+    "revision": "247f2a50408ed7b821ea",
+    "url": "/static/js/main.02c04d35.chunk.js"
+  },
+  {
+    "revision": "68791116a82c080252f0",
+    "url": "/static/js/runtime~main.f9e6c4ba.js"
+  }
+]);
